3f528b8541b6d195db5bda173cbc12cea8e88f06
Tomb520 <tomb520@gmail.com>
2025-10-27 06:48:08 -0400

    Remove CSP restrictions - use Next.js defaults

--- next.config.js
+++ next.config.js
@@ -13,19 +13,6 @@ const nextConfig = {
     ],
     unoptimized: true,
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "img-src * data: blob: 'unsafe-inline';",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
